45fce7d83439a857fb4674f314982b8f78d53908
Andrew Nguyen <nguyenandrew137@protonmail.com>
2025-04-30 23:23:58 -0400

    feat: create a pipe mechanism
    refactor: separate the fg and bg create_process, since one is
    essentially a wrapper

--- skin.c
+++ skin.c
@@ -17,9 +17,9 @@
 #include "skin_lex.c"
 #include "skin.h"
 
-enum
+struct pipefd_t
 {
-    CREATE_PROCESS_BACKGROUND = 1 << 0,
+    int pipefd[2];
 };
 
 enum execute_flag
@@ -27,40 +27,48 @@ enum execute_flag
     EXECUTE_CAPTURE_OUT = 1 << 0,
     EXECUTE_LPAREN = 1 << 1,
     EXECUTE_RPAREN = 1 << 2,
+    EXECUTE_BACKGROUND = 1 << 3,
 };
 
 extern char **environ;
 
-#define STDFD_NO_OVERRIDE ((struct stdfd){.in=-1, .out=-1, .err=-1});
+#define STDFD_NO_OVERRIDE_BASE {.in=-1, .out=-1, .err=-1, .close = NULL}
+#define STDFD_NO_OVERRIDE ((struct stdfd)STDFD_NO_OVERRIDE_BASE)
 struct stdfd
 {
     int in;
     int out;
     int err;
+    int *close;
+    size_t close_size;
 };
+struct stdfd const no_override_stdfd = STDFD_NO_OVERRIDE_BASE;
 
 
 char *
-find_env(struct string_arr *envp, char *name, size_t name_size)
+find_env(
+        struct string_arr const *restrict envp,
+        char const *restrict name,
+        size_t name_size)
 {
-    array_null_foreach(envp, e)
+    array_null_foreach(
+            unsafe_cast(struct string_arr *, envp), e)
     {
         if(strstr(*e, name) == *e) return *e + name_size + 1;
     }
     return NULL;
 }
 
-// WARN: create_process
+// WARN: create_process_bg
 // name must be null terminated
 // args->buffer must be null terminated
 // envp->buffer must be null terminated
 pid_t
-create_process(
-        char *restrict name,
-        struct string_arr *restrict args,
-        struct string_arr *restrict envp,
-        struct stdfd *restrict ofd,
-        uint32_t flags)
+create_process_bg(
+        char const *restrict name,
+        struct string_arr const *restrict args,
+        struct string_arr const *restrict envp,
+        struct stdfd ofd)
 {
     pid_t pid;
     posix_spawn_file_actions_t file_actions;
@@ -120,32 +128,43 @@ EXIT_CREATE:
     if(posix_spawn_file_actions_init(&file_actions))
         die("failed to init spawn file actions");
 
-    if(ofd->out != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd->out, STDOUT_FILENO))
-        die("failed to init spawn file actions file descriptor");
-    if(ofd->in != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd->in, STDIN_FILENO))
-        die("failed to init spawn file actions file descriptor");
-    if(ofd->err != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd->err, STDERR_FILENO))
-        die("failed to init spawn file actions file descriptor");
+    if(ofd.out != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd.out, STDOUT_FILENO))
+        die("failed to init spawn file actions");
+    if(ofd.in != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd.in, STDIN_FILENO))
+        die("failed to init spawn file actions");
+    if(ofd.err != -1 && posix_spawn_file_actions_adddup2(&file_actions, ofd.err, STDERR_FILENO))
+        die("failed to init spawn file actions");
+
+    for(size_t i = 0; i < ofd.close_size; i++)
+    {
+        if(ofd.close[i] != -1 && posix_spawn_file_actions_addclose(&file_actions, ofd.close[i]))
+            die("failed to init spawn file actions");
+    }
 
     if(posix_spawn(&pid, binary, &file_actions, NULL, args->buffer, envp->buffer) || pid == -1)
         die("failed to spawn child");
     if(posix_spawn_file_actions_destroy(&file_actions)) die("failed to destroy spawn file actions");
 
-    if(flags & CREATE_PROCESS_BACKGROUND)
-    {
-        return(pid);
-    }
-    else
-    {
-        waitpid(pid, NULL, 0);
-        return(0);
-    }
+    return(pid);
 }
 
+pid_t
+create_process_fg(
+        char const *restrict name,
+        struct string_arr const *restrict args,
+        struct string_arr const *restrict envp,
+        struct stdfd ofd)
+{
+    pid_t pid = create_process_bg(name, args, envp, ofd);
+    return(waitpid(pid, NULL, 0));
+}
+
+// TODO: add bg processes to a stack
 int
 execute(
         struct lex_state *restrict ls,
         struct string_arr *restrict envp,
+        struct stdfd ov_stdfd,
         char **restrict output,
         uint32_t flags)
 {
@@ -153,15 +172,13 @@ execute(
     int64_t res;
     struct string temp_str = {0};
 
-    /* fork prep */
+    /* spawn prep */
     struct string_arr args = {0};
     array_init(&args, 8);
 
     struct string name = {0};
     array_init(&name, 8);
 
-    uint32_t child_flags = 0;
-    struct stdfd ov_stdfd = STDFD_NO_OVERRIDE;
     int pipefd[2];
 
     /* parse */
@@ -188,10 +205,11 @@ execute(
         else if(res == TOKEN_LPAREN)
         {
             array_push(&args, NULL);
-            if(execute(ls, envp, &array_end_idx(&args, -1),
+            if(execute(ls, envp, no_override_stdfd, &array_end_idx(&args, -1),
                         EXECUTE_CAPTURE_OUT | EXECUTE_RPAREN) == -1)
             {
-                retval = -1; goto EXIT;
+                retval = -1;
+                goto EXIT;
             }
         }
         else
@@ -251,25 +269,100 @@ execute(
     {
         goto EXIT;
     }
-    else if(strcmp(name.buffer, "pipe") == 0)
+    else if(strcmp(name.buffer, "pipe") == 0 || strcmp(name.buffer, "|") == 0)
     {
+        if(num_args < 2) {debug_named("pipe: requires >=2 arguments"); retval = -1; goto EXIT;}
+
+        struct pipefd_t *pipefd_arr = calloc(num_args - 1, sizeof(*pipefd_arr));
+        if(pipefd_arr == NULL) die("failed to alloc");
+
+        pid_t *pid_arr = calloc(num_args, sizeof(*pid_arr));
+        if(pid_arr == NULL) die("failed to alloc");
+
+        struct stdfd new_ov_stdfd = ov_stdfd;
+        new_ov_stdfd.close = (int *)pipefd_arr; // WARN: we ignore the ov_stdfd.close
+        new_ov_stdfd.close_size = 0;
+        int temp_fds[2] = {0};
+
+        for(size_t i = 0; i < num_args; i++)
+        {
+            // pipe
+            if(i != num_args - 1 && pipe(pipefd_arr[i].pipefd) == -1) die("pipe: failed to create");
+
+            if(i != num_args - 1)
+            {
+                new_ov_stdfd.out = pipefd_arr[i].pipefd[1];
+                // disable close
+                temp_fds[1] = pipefd_arr[i].pipefd[1];
+                pipefd_arr[i].pipefd[1] = -1;
+            }
+            else
+            {
+                new_ov_stdfd.out = ov_stdfd.out;
+            }
+
+            if(i != 0)
+            {
+                new_ov_stdfd.in = pipefd_arr[i - 1].pipefd[0];
+                // disable close
+                temp_fds[0] = pipefd_arr[i - 1].pipefd[0];
+                pipefd_arr[i - 1].pipefd[0] = -1;
+            }
+            else
+            {
+                new_ov_stdfd.in = ov_stdfd.in;
+            }
+
+            // setup the close array
+            new_ov_stdfd.close_size =
+                (i == num_args - 1)
+                ? (i * 2)
+                : ((i + 1) * 2);
+
+            // WARN: spawn
+            // multiple executes in a row, works because:
+            // - execute's lexing is synchronous
+            // - new_ov_stdfd is copied
+            // - close gets used immediately when executing
+            struct lex_state ls2 = {0};
+            lex_init(&ls2, args.buffer[i + 1], strlen(args.buffer[i + 1]));
+            pid_arr[i] = execute(&ls2, envp, new_ov_stdfd, NULL, EXECUTE_BACKGROUND);
+            if(pid_arr[i] == -1) { die("failed to pipe"); } // TODO: better resolve
+
+            // reset pipefd_arr to original state
+            if(i != 0) pipefd_arr[i - 1].pipefd[0] = temp_fds[0];
+            if(i != num_args - 1) pipefd_arr[i].pipefd[1] = temp_fds[1];
+        }
+
+        // cleanup
+        for(size_t i = 0; i < num_args - 1; i++)
+        {
+            close(pipefd_arr[i].pipefd[0]);
+            close(pipefd_arr[i].pipefd[1]);
+            waitpid(pid_arr[i], NULL, 0);
+        }
+        free(pipefd_arr);
+        free(pid_arr);
         goto EXIT;
     }
     else if(strcmp(name.buffer, "exit") == 0)
     {
+        // TODO: clean exit
         exit(0);
     }
 
     // prep child
+    pid_t pid = 0;
     if(flags & EXECUTE_CAPTURE_OUT)
     {
         if(pipe(pipefd) == -1) die("failed to spawn pipe");
         ov_stdfd.out = pipefd[1];
-        child_flags |= CREATE_PROCESS_BACKGROUND;
     }
 
     // spawn
-    pid_t pid = create_process(name.buffer, &args, envp, &ov_stdfd, child_flags);
+    if(flags & EXECUTE_CAPTURE_OUT) pid = create_process_bg(name.buffer, &args, envp, ov_stdfd);
+    else if(flags & EXECUTE_BACKGROUND) retval = pid = create_process_bg(name.buffer, &args, envp, ov_stdfd);
+    else pid = create_process_fg(name.buffer, &args, envp, ov_stdfd);
 
     // post child
     if(flags & EXECUTE_CAPTURE_OUT)
@@ -310,7 +403,7 @@ read_line(
 {
     struct lex_state ls = {0};
     lex_init(&ls, prompt, prompt_size);
-    execute(&ls, envp, NULL, 0);
+    execute(&ls, envp, no_override_stdfd, NULL, 0);
     fflush(out);
     return(getline(line, size, in));
 }
@@ -333,7 +426,7 @@ main(int argc, char **argv)
 
     struct string_arr envp = {0};
     {
-        array_init(&envp, 16);
+        array_init(&envp, 128);
         char **e = environ;
         while(*e != NULL) {
             if(strstr(*e, "HOME") == *e) strncpy(builtin_home, *e + sizeof("HOME"), PATH_MAX);
@@ -361,19 +454,16 @@ main(int argc, char **argv)
 
     /* main */
     struct lex_state ls = {0};
-
-    // // DEBUG: static tests
-    // char temp_buf[] = "(echo '(hi \\(lmao\\) \"bro\") (date -I) (echo kjfd) klsjdflkjdsal)";
-    // lex_init(&ls, temp_buf, sizeof(temp_buf));
-    // debug_named("retval: %d", execute(&ls, &envp, NULL, 0));
+    // char temp_line[] = "| ls '(head -15) '(tail -5) '(head -4)";
+    // lex_init(&ls, temp_line, sizeof(temp_line));
+    // debug_named("retval: %d", execute(&ls, &envp, no_override_stdfd, NULL, 0));
 
     char *line = NULL;
     size_t line_size = 0;
     while(read_line(prompt, prompt_size, &line, &line_size, &envp, stdin, stdout) != -1)
     {
         lex_init(&ls, line, line_size);
-        execute(&ls, &envp, NULL, 0);
-        // debug_named("retval: %d", execute(&ls, &envp, NULL, 0));
+        execute(&ls, &envp, no_override_stdfd, NULL, 0);
     }
     free(line);
     array_destroy(&envp);
--- skin.h
+++ skin.h
@@ -8,4 +8,6 @@
 #define debug_named(...) debug(PROG_NAME": " __VA_ARGS__)
 #define die(...) {debug_named(__VA_ARGS__); fputc('\n', stderr); exit(-1);}
 
+#define unsafe_cast(t, x) ((t)(x))
+
 #endif // SKIN_H
